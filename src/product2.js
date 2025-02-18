5f64c7f30d855ae6ac2562daab819a28ccf96299
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-02-18 21:50:40 +0100

    mainly focus css added

---
+++ src/product2.js
@@ -0,0 +1,7 @@
+document.getElementById('productVideo').addEventListener('click', function () {
+  window.open(
+    'src/videos/a00002-960x960.mp4',
+    '_blank',
+    'width=960,height=960'
+  );
+});
--- src/webshop.js
+++ src/webshop.js
@@ -399,11 +399,3 @@ function emptyBasket(conf = true) {
     showCheckout();
   }
 }
-
-document.getElementById('productVideo').addEventListener('click', function () {
-  window.open(
-    'src/videos/a00002-960x960.mp4',
-    '_blank',
-    'width=960,height=960'
-  );
-});
