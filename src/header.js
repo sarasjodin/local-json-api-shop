637f1b5d5456d64f5d2698021f7e56f645e180e7
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-02-18 16:23:49 +0100

    removed avif and webp favicons

--- src/header.js
+++ src/header.js
@@ -1,12 +1,21 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const navbarContainer = document.querySelector(".navbar-container");
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navbarContainer = document.querySelector('.navbar-container');
 
   if (hamburgerMenu && navbarContainer) {
-    hamburgerMenu.addEventListener("click", () => {
-      navbarContainer.classList.toggle("show");
+    hamburgerMenu.addEventListener('click', () => {
+      navbarContainer.classList.toggle('show');
     });
   }
 });
+
+document
+  .querySelector('.hamburger-menu')
+  .addEventListener('click', function () {
+    this.setAttribute(
+      'aria-label',
+      this.classList.toggle('open') ? 'Stäng meny' : 'Öppna meny'
+    );
+  });
