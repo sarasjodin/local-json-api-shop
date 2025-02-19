e42538f9fe7d1bec23abb215acb70bd0754053f3
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-02-19 09:30:46 +0100

    accessibility changes and general improvements

--- src/product2.js
+++ src/product2.js
@@ -1,7 +1,9 @@
-document.getElementById('productVideo').addEventListener('click', function () {
-  window.open(
-    'src/videos/a00002-960x960.mp4',
-    '_blank',
-    'width=960,height=960'
-  );
-});
+document
+  .getElementById('product2-video')
+  .addEventListener('click', function () {
+    window.open(
+      'src/videos/a00002-960x960.mp4',
+      '_blank',
+      'width=960,height=960'
+    );
+  });
